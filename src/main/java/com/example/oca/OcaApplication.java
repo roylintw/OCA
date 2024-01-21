0ad79a407d191eea041b7a3e15c9f2f7b67f11ec
Roy Lin <bizroylin@gmail.com>
2024-01-21 21:10:10 +0800

    Create Project

---
+++ src/main/java/com/example/oca/OcaApplication.java
@@ -0,0 +1,13 @@
+package com.example.oca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oca/OcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
