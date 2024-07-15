cf734f4aab4a294310abc0d616952e12692ae23c
roylin1995 <bizroylin@gmail.com>
2024-07-15 18:27:29 +0800

    [feature]第188題

---
+++ src/main/java/com/service/OCA188.java
@@ -0,0 +1,22 @@
+package com.service;
+
+public class OCA188 {
+
+    public static void doSum(Integer x, Integer y) {
+        System.out.println("Integer sum is " + (x + y));
+    }
+
+    public static void doSum(double x, double y) {
+        System.out.println("double sum is " + (x + y));
+
+    }
+
+    public static void doSum(float x, float y) {
+        System.out.println("float sum is " + (x + y));
+    }
+
+    public static void main(String[] args) {
+        doSum(10, 20);
+        doSum(10.0, 20.0);
+    }
+}
