9e99984e4e21bbd8dd6cf7f1f4f9d53d229dde9b
roylin1995 <bizroylin@gmail.com>
2024-08-25 15:43:57 +0800

    [feature]第2題

---
+++ src/main/java/com/service/OCA2.java
@@ -0,0 +1,15 @@
+package com.service;
+
+public class OCA2 {
+    public static void main(String[] args) {
+        String value = "";
+        String condition = "";
+        switch (value) {
+            case "1":
+                System.out.println("1");
+//            case condition:
+//                System.out.println("2");
+                break;
+        }
+    }
+}
